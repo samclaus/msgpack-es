7bb1985cc088a93d003f0fcf9c2f35bd9219b129
Sam Claus <samterainsights@gmail.com>
2018-10-06 16:39:55 -0400

    Setup project

---
+++ Decoder.ts
@@ -0,0 +1,13 @@
+/**
+ * Class for decoding arrays, objects, and primitives to msgpack
+ * format. You can create indepently configured instances, but you will
+ * most likely want to simply use the static methods and interfact with
+ * the default/global Encoder.
+ */
+export class Decoder {
+
+    decode<T>(data: ArrayBuffer | Uint8Array): T {
+
+    }
+
+}
---
+++ Encoder.ts
@@ -0,0 +1,13 @@
+/**
+ * Class for encoding arrays, objects, and primitives to msgpack
+ * format. You can create indepently configured instances, but you will
+ * most likely want to simply use the static methods and interfact with
+ * the default/global Encoder.
+ */
+export class Encoder {
+
+    encode(): Uint8Array {
+        
+    }
+
+}
