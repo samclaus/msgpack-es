2b85cb94f37cb853db76f7b78904744a9284b23b
Sam Claus <samclaus1999@gmail.com>
2019-02-09 18:31:34 -0500

    Remove "msgpack" global

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,2 @@
-import { Encoder } from "./Encoder";
-import { Decoder } from "./Decoder";
-
 export * from "./Encoder";
 export * from "./Decoder";
-
-window["msgpack"] = {
-    Encoder: Encoder,
-    Decoder: Decoder
-};
