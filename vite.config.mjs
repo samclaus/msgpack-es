fe2003702bb262fbc3ce2a71f1328134bb79efac
Sam Claus <samclaus@protonmail.com>
2024-04-06 00:27:10 -0400

    Modernize package setup

--- vite.config.mjs
+++ vite.config.mjs
@@ -8,6 +8,7 @@ export default defineConfig({
     build: {
         lib: {
             formats: ['es'],
+            fileName: () => 'index.mjs', // Need function otherwise it produces 'index.mjs.js'!
             entry: resolve(__dirname, 'src/index.ts'),
         },
     },
