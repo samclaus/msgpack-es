1a21f9f595dafae39cce331865ecaa520120088b
Daniel Gavrilov <dgavrilov95@gmail.com>
2024-06-14 08:18:44 +0200

    Test nested encoding & decoding

--- src/general.test.ts
+++ src/general.test.ts
@@ -1,5 +1,5 @@
 import { it, expect } from 'vitest';
-import { encode, decode, DECODE_OPTS } from '.';
+import { encode, decode, DECODE_OPTS, registerExtension } from '.';
 
 it('Encode+decode produces the original input', () => {
     const data = {
@@ -85,5 +85,34 @@ it("Respects 'nilValue' and omits undefined map values", () => {
 
     DECODE_OPTS.nilValue = origNilValue;
 });
+
+it("Handles nested calls to encode and decode", () => {
+    class CustomA {
+        constructor(readonly valueA: CustomB) {}
+    }
+    class CustomB {
+        constructor(readonly valueB: number) {}
+    }
+
+    registerExtension(
+        1,
+        CustomA,
+        (custom) => encode(custom.valueA),
+        (buffer) => new CustomA(decode(buffer))
+    )
+    registerExtension(
+        2,
+        CustomB,
+        (custom) => new Uint8Array([custom.valueB]),
+        (buffer) => new CustomB(buffer[0])
+    )
+
+    const data = new CustomA(new CustomB(0))
+    const output = decode(encode(data)) as any
+
+    expect(output).toBeInstanceOf(CustomA)
+    expect(output.valueA).toBeInstanceOf(CustomB)
+    expect(output.valueA.valueB).toStrictEqual(0)
+})
 
 // TODO: more tests, refine tests
